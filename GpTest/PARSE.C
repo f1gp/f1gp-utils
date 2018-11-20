3213847791a0ff899e242f28b1c926b7292b0f03
sdidit <sdidit@gmail.com>
2018-11-20 20:41:54 +0100

    Moved hook inits to separate procs.

--- GpTest/PARSE.C
+++ GpTest/PARSE.C
@@ -43,9 +43,9 @@ short atoi(register const char far *p);
 */
 
 char title_msg[] =
-     "@(#)"
-     "GpTest " VERSION " - Grand Prix/World Circuit Testing Support Tool.\n"
-     "Copyright (c) Rene Smit 2018 - All Rights Reserved.\n\n";
+    "@(#)"
+    "GpTest " VERSION " - Grand Prix/World Circuit Testing Support Tool.\n"
+    "Copyright (c) Rene Smit 2018 - All Rights Reserved.\n\n";
 
 /*---------------------------------------------------------------------------
 ** Functions
@@ -53,102 +53,102 @@ char title_msg[] =
 
 int
 parse(
-     void
+    void
 ) {
-     register int   i;
-     int            option_next;
-     int            n;
+    register int   i;
+    int            option_next;
+    int            n;
 
-     display_msg(&title_msg[WHAT_OFFSET]);
+    display_msg(&title_msg[WHAT_OFFSET]);
 
-     for (i = 0; i <= cmd_line_len; i++) {
-          if ( cmd_line[i] == '\r' || cmd_line[i] == '\n' ||
-               cmd_line[i] == ' '  || cmd_line[i] == '\t'
-          ) {
-               cmd_line[i] = '\0';
-          }
-     }
+    for (i = 0; i <= cmd_line_len; i++) {
+        if ( cmd_line[i] == '\r' || cmd_line[i] == '\n' ||
+             cmd_line[i] == ' '  || cmd_line[i] == '\t'
+         ) {
+             cmd_line[i] = '\0';
+         }
+    }
 
-     option_next = FALSE;
-     while (cmd_line_len--) {
-          if (option_next) {
-               option_next = FALSE;
+    option_next = FALSE;
+    while (cmd_line_len--) {
+        if (option_next) {
+            option_next = FALSE;
 
-               /*
-               ** Don't use switch as indirect table gets it wrong for a COM file.
-               */
-               if (*cmd_line == 'f') {
-                    fname_ptr = cmd_line + 1;
-                    if (cmd_line_len == 0 || *fname_ptr == '\0') {
-                         display_msg("GpTest: missing log filename.\n");
-                         Usage();
-                         return FALSE;
-                    }
-               }
-               else if (*cmd_line == 'l') {
-                    n = atoi(&cmd_line[1]);
-                    if (n >= 1 && n <= 26) {
-                         limit_cars_race = n;
-                    }
-                    else {
-                         display_msg("GpPerf: -l value should be between 1 and 26 cars.\n");
-                         return FALSE;
-                    }
-               }
-               else if (*cmd_line == 'u') {
-                    unload_flag = TRUE;
-                    return TRUE;
-               }
-               else {
-                    Usage();
-                    return FALSE;
-               }
-          }
-          else if (*cmd_line == '-' || *cmd_line == '/') {
-               option_next = TRUE;
-          }
-          ++cmd_line;
-     }
+            /*
+            ** Don't use switch as indirect table gets it wrong for a COM file.
+            */
+            if (*cmd_line == 'f') {
+                fname_ptr = cmd_line + 1;
+                if (cmd_line_len == 0 || *fname_ptr == '\0') {
+                     display_msg("GpTest: missing log filename.\n");
+                     Usage();
+                     return FALSE;
+                }
+            }
+            else if (*cmd_line == 'l') {
+                n = atoi(&cmd_line[1]);
+                if (n >= 1 && n <= 26) {
+                     limit_cars_race = n;
+                }
+                else {
+                     display_msg("GpPerf: -l value should be between 1 and 26 cars.\n");
+                     return FALSE;
+                }
+            }
+            else if (*cmd_line == 'u') {
+                unload_flag = TRUE;
+                return TRUE;
+            }
+            else {
+                Usage();
+                return FALSE;
+            }
+        }
+        else if (*cmd_line == '-' || *cmd_line == '/') {
+            option_next = TRUE;
+        }
+        ++cmd_line;
+    }
 
-     return TRUE;
+    return TRUE;
 }
 
 void
 Usage(
      void
 ) {
-     display_msg(   "Usage: GpTest [-lN] [-h?] [-u] [-f(filename)]\n"
-                    "       -f(name)  Specify log filename's location.\n"
-                    "       -l(N)     Limit race to N cars.\n"
-                    "       -h,-?     This help message.\n"
-                    "       -u        Unload TSR.\n"
-               );
+    display_msg("Usage: GpTest [-lN] [-h?] [-u] [-f(filename)]\n"
+                "       -f(name)  Specify log filename's location.\n"
+                "       -l(N)     Limit race to N cars.\n"
+                "       -h,-?     This help message.\n"
+                "       -u        Unload TSR.\n"
+    );
 }
 
 void
 display_msg(
-     char near *msg                     /* In Msg to display               */
+    char near *msg                     /* In Msg to display               */
 ) {
-     while (*msg) {
-          if (*msg == '\n') {
-               display_chr('\r');
-          }
-          display_chr(*msg++);
-     }
+    while (*msg) {
+        if (*msg == '\n') {
+            display_chr('\r');
+        }
+        display_chr(*msg++);
+    }
 }
 
 /*lint -e789 Ignore assigning auto to static */
 
 void
 display_chr(
-     char c                             /* In Character to display         */
+    char c                             /* In Character to display         */
 ) {
-     char cs[2];
+    char cs[2];
 
-     cs[0]     = c;
-     cs[1]     = '$';
-     msg_text  = cs;
-     wrt_msg();
+    cs[0]     = c;
+    cs[1]     = '$';
+    msg_text  = cs;
+    wrt_msg();
 }
 
 /*lint +e789 Ignore assigning auto to static */
@@ -157,34 +157,34 @@ display_chr(
 
 short
 atoi(
-     register const char far *p         /* In  Pointer to ASCII string     */
+    register const char far *p         /* In  Pointer to ASCII string     */
 ) {
-     register int n;
-     register int f;
+    register int n;
+    register int f;
 
-     n = 0;
-     f = 0;
-     for ( ; ; p++) {
-          switch (*p) {
-          case ' ':
-          case '\t':
-               continue;
-          case '-':
-               f++;
-               /*lint fall throught */
-          case '+':
-               p++;
-               break;
+    n = 0;
+    f = 0;
+    for ( ; ; p++) {
+        switch (*p) {
+        case ' ':
+        case '\t':
+            continue;
+        case '-':
+            f++;
+            /*lint fall throught */
+        case '+':
+            p++;
+            break;
 
-          default:
-               break;
-          }
-          break;
-     }
-     while (*p >= '0' && *p <= '9') {
-          n = n * 10 + *p++ - '0';
-     }
-     return (f ? -n : n);
+        default:
+            break;
+        }
+        break;
+    }
+    while (*p >= '0' && *p <= '9') {
+        n = n * 10 + *p++ - '0';
+    }
+    return (f ? -n : n);
 }
 
 /*---------------------------------------------------------------------------
